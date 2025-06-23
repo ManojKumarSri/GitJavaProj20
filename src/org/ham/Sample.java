a5bb265fb7d54dcf50909cf713685b0655c8cca1
ManojKumarSri <kumarsrinivasanmanoj@gmail.com>
2025-06-23 12:59:33 +0530

    Anu added few methods

--- src/org/ham/Sample.java
+++ src/org/ham/Sample.java
@@ -1,12 +1,18 @@
 package org.ham;
 
 public class Sample {
+	
+	private void m1() {
+		System.out.println("Method---1");
+	}
 
 	public static void main(String[] args) {
 
+		Sample s = new Sample();
+		s.m1();
+		
 		int a = 10;
 		System.out.println(a);
 
 	}
 }
-
