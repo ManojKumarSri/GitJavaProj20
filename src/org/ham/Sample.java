87fa492dd3b4df294a501ee067a93fce92fe63b8
ManojKumarSri <kumarsrinivasanmanoj@gmail.com>
2025-06-24 15:25:31 +0530

    Banu done her code

--- src/org/ham/Sample.java
+++ src/org/ham/Sample.java
@@ -2,11 +2,14 @@ package org.ham;
 
 public class Sample {
 
+	public static String name = "SmartSkills";
+
 	public static void main(String[] args) {
 
 		int a = 10;
 		System.out.println(a);
 
+		System.out.println(name);
+
 	}
 }
-
